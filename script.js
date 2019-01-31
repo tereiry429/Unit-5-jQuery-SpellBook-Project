2977ad1ff9771e7f9a3c6c5b87b6e28287c63ee9
tereiry429 <35636506+tereiry429@users.noreply.github.com>
2019-01-31 09:16:55 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".light-switch").click(function(){
+    $("#light-bulb").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".mind-erase").click(function(){
+   $("#erase").hide();
+});
+
+
+
+//Spell #3
+
+$(".float").click(function(){
+   $("#feather").slideToggle();
+});
+
+
+
